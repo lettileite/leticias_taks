ce9b394912ef249909df65e36f56fe006a2f8643
lettileite <34384710+lettileite@users.noreply.github.com>
2020-08-17 09:17:24 +1200

    first_commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <?php
+            include 'external_libraries.html'
+    ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>adaLove</title>
+</head>
+<body>
+<div>
+        <br>
+        <br>
+        <br>
+        <img position='center' src="img/logoNav.png" width="148" height="50" alt="adaLoveLogo"/> 
+</div>
+
+<div>
+<footer>
+    <div id="footer" class="footer w3-container w3-white">
+<hr>
+
+<div style="overflow:auto">
+  <div class="bottomad">
+    <div id="snhb-bottom_medium_rectangle-0" style="padding:0 10px 10px 0;float:left;width:auto;"></div>
+    <div id="snhb-right_bottom_medium_rectangle-0" style="padding:0 10px 10px 0;float:left;width:auto;"></div>
+  </div>
+</div>
+
+<div class="container-fluid" class="w3-row-padding w3-center w3-small" style="margin:0 -16px">
+    <div class="w3-col 13 m3 s12">
+            <a class="w3-button w3-light-grey w3-block" href="XXX" target="_blank" 
+            style="white-space:nowrap;text-decoration:none; margin-top:1px;margin-bottom:1px"> About us</a>    
+        </div>  
+        <div class="w3-col 13 m3 s12">
+        <a class="w3-button w3-light-grey w3-block" href="XXX" target="_blank" 
+            style="white-space:nowrap;text-decoration:none; margin-top:1px;margin-bottom:1px"> Contact us</a>        
+        </div>
+        <div class="w3-col 13 m3 s12">
+            <a class="w3-button w3-light-grey w3-block" href="XXX" target="_blank" 
+            style="white-space:nowrap;text-decoration:none; margin-top:1px;margin-bottom:1px"> Privacy</a>        
+        </div>
+        <div class="w3-col 13 m3 s12">
+            <a  class="w3-button w3-light-grey w3-block" href="XXX" target="_blank" 
+            style="white-space:nowrap;text-decoration:none; margin-top:1px;margin-bottom:1px"> Terms & Conditions</a>        
+        </div>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
